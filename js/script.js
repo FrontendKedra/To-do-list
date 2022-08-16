283db4e7d19e1f2d275fe2dab2f3f712d0fdce16
Szymon Kedra <s5zee@wp.pl>
2022-08-16 11:00:41 +0200

    toggleTaskDone changed based on immutability

--- js/script.js
+++ js/script.js
@@ -18,9 +18,13 @@
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            { ...tasks[taskIndex], done: !tasks[taskIndex].done },
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
-    }
+    };
 
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
